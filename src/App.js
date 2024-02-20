627e39e0b56ae8dfd32a5f6fe8682fafc8b5ad9f
tarunbly171098 <118833425+tarunbly171098@users.noreply.github.com>
2024-02-20 19:27:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React2
+          Hello Mohammad Naeem
         </a>
       </header>
     </div>
